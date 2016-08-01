0c72fb2d5bd9b0806789ec92578f0c15ee28a722
alexthedar <alex.castaneda@gmail.com>
2016-07-31 18:10:37 -0700

    setup complete

---
+++ SwifttrisPlus/SwifttrisPlus/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  SwifttrisPlus
+//
+//  Created by Alexandar on 7/31/16.
+//  Copyright (c) 2016 Alexandar. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ SwifttrisPlus/SwifttrisPlus/GameViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GameViewController.swift
+//  SwifttrisPlus
+//
+//  Created by Alexandar on 7/31/16.
+//  Copyright (c) 2016 Alexandar. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
